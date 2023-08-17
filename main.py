3c058b3b7edd078aed2ec694fbb35dc24e4ce7f0
Joey <37638087+Yunikuna-Joey@users.noreply.github.com>
2023-08-16 20:41:13 -0700

    Setting up environment variables
    quick setup of virtual environment and its variables

---
+++ main.py
@@ -0,0 +1,12 @@
+# remember to run pip install discord.py 
+import discord 
+from discord.ext import commands 
+
+# here remember to run pip install python-dotenv(?)
+from dotenv import load_dotenv
+import os 
+
+# here remember to run pip install yfinance in cmd for these imports  
+import yfinance as yf 
+
+print('Hello World!') 
